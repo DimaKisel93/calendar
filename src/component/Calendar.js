86c50844bfd351bb86ad74fa9dd7cc033f07f6a8
Dima Kisel <dimakisel333@gmail.com>
2020-11-12 17:02:51 +0200

    Calendar rows and columns done

--- src/component/Calendar.js
+++ src/component/Calendar.js
@@ -1,5 +1,45 @@
+import React, {useState, useEffect} from "react";
+import moment from "moment";
+
 function Calendar() {
-  return (<div>Calendar</div>) ;
+    const [calendar,setCalendar] = useState([]);
+    const [value, setValue] = useState(moment());
+    const startDay = value.clone().startOf("month");
+    const endDay = value.clone().endOf("month");
+    
+    useEffect(() => {
+        const day = startDay.clone().subtract(1,"day");
+        const a = [];
+        while(day.isBefore(endDay, "day")){
+            a.push(
+                Array(7)
+                .fill(0)
+                .map(() => day.add(1, "day").clone())
+            );
+        }
+        setCalendar(a);
+    }, [value])
+
+   
+    return (
+        <div className="calendar">
+            {calendar.map((week) => (
+                <div> 
+                {
+                    week.map((day) => (
+                    <div className="day"
+                        onClick={() => setValue(day)}
+                    >
+                        <div className={value.isSame(day , "day")}>
+                            {day.format("D").toString()}
+                        </div>
+                    </div> 
+                    ))}
+                </div>
+            ))}
+        </div>
+    );
 }
+
 
 export default Calendar
